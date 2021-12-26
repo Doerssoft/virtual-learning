bf08b1d5d37e5aa6f38201f88b7512395053b6e2
Gaurav <gauravzaiswal@gmail.com>
2021-12-26 15:38:18 +0545

    added forget password feature

--- virtual_learning/virtual_learning/urls.py
+++ virtual_learning/virtual_learning/urls.py
@@ -17,7 +17,11 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('update-password/', auth_views.PasswordChangeView.as_view(template_name='users/changepassword.html'), name='password_change'),
-    path('reset-password/', auth_views.PasswordChangeDoneView.as_view(template_name='users/passwordchangesuccess.html'), name='password_change_done')
+    path('update-password-done/', auth_views.PasswordChangeDoneView.as_view(template_name='users/passwordchangesuccess.html'), name='password_change_done'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/passwordreset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/passwordresetdone.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/passwordresetconfirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='users/passwordresetcomplete.html'), name='password_reset_complete'),
     # path('social/', include('allauth.socialaccount.urls')),
     # path('account/', include('allauth.urls')),
 ]
