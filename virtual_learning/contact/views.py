5757b3a77fb4769b2476bdf732049688e1e55369
Gaurav <gauravzaiswal@gmail.com>
2022-01-03 14:27:45 +0545

    added try except to send email

--- virtual_learning/contact/views.py
+++ virtual_learning/contact/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.core.mail import send_mail
+from django.core.mail import BadHeaderError, send_mail
 from django.conf import settings
 
 from .forms import ContactForm
@@ -11,7 +11,6 @@ from .forms import ContactForm
 # @login_required(login_url='login')
 def contact(request):
     template_name = 'contact/partners.html'
-
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
@@ -23,20 +22,24 @@ def contact(request):
             response["phone"] = form.cleaned_data["phone"]
             response["address"] = form.cleaned_data["address"]
             response["message"] = form.cleaned_data["message"]
-            send_mail(
-                'Soyah --new contact message received', # subject
-                # email message/body 
-                "A new form has been recorded with following details:"+"\n"+ "\n"+
-                "Full Name: {}".format(response['full_name'])+ "\n" + 
-                "Users Email: {}".format(response['email']) + "\n" +
-                "Phone: {}".format(response['phone']) + "\n" +
-                "address: {}".format(response['address']) + "\n" +
-                "Message: {}".format(response['message']) + "\n" 
-                ,
-                settings.EMAIL_HOST_USER,  # sender
-                ['jr.gaurav2015@gmail.com'], # receiver
-                fail_silently=False,
-            )
+            
+            try:
+                send_mail(
+                    'Soyah --new contact message received', # subject
+                    # email message/body 
+                    "A new form has been recorded with following details:"+"\n"+ "\n"+
+                    "Full Name: {}".format(response['full_name'])+ "\n" + 
+                    "Users Email: {}".format(response['email']) + "\n" +
+                    "Phone: {}".format(response['phone']) + "\n" +
+                    "address: {}".format(response['address']) + "\n" +
+                    "Message: {}".format(response['message']) + "\n" 
+                    ,
+                    settings.EMAIL_HOST_USER,  # sender
+                    ['jr.gaurav2015@gmail.com'], # receiver
+                    fail_silently=False,
+                )
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
 
             messages.success(request, ("Response has been recorded thank you."))
             return render(request, 'users/index.html')
